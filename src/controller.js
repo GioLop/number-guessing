13dcccc54d04dedcac3ddb2ad4c7c85be6ef85a4
Giovanni López <giovanni.helion@gmail.com>
2024-09-13 11:05:13 -0500

    Split steps

--- src/controller.js
+++ src/controller.js
@@ -1,57 +1,50 @@
 const readLineSync = require('readline-sync');
+const { LEVELS } = require('./lib/constanst.lib');
 
-const main = () => {
-  const thinkedNumber = Math.floor(Math.random() * 100) + 1;
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 
-  console.log(thinkedNumber);
-
-  console.log(`Welcome to the Number Guessing Game!`);
+const greet = () => {
+  console.log(`\nWelcome to the Number Guessing Game!`);
   console.log(`I'm thinking of a number between 1 and 100.`);
   console.log(`Based on the difficult level you'll have determinated chances to guess the correct number.`);
+};
+
+const chooseLevel = () => {
   console.log(`\nPlease select the difficulty level:`);
   
-  const levels = {
-    1: {
-      name: 'Easy',
-      chances: 10
-    },
-    2: {
-      name: 'Medium',
-      chances: 5
-    },
-    3: {
-      name: 'Hard',
-      chances: 3
-    }
-  };
-
-  for (let key in levels) {
-    console.log(`${key}. ${levels[key].name} (${levels[key].chances} chances)`);
+  for (let key in LEVELS) {
+    console.log(`${key}. ${LEVELS[key].name} (${LEVELS[key].chances} chances)`);
   }
   
   const choice = readLineSync.question(('\nEnter your choice: '));
-  const levelsKeys = Object.keys(levels);
+  const levelsKeys = Object.keys(LEVELS);
 
   if (!levelsKeys.includes(choice)) {
     console.log(`\nOption ${choice} is not valid, bye!`);
     return;
   }
-  
-  console.log(`\nGreat! You have selected the ${levels[choice].name} difficult level.`);
-  console.log(`Let's start the game!\n`);
 
-  let chances = levels[choice].chances;
+  return LEVELS[choice];
+};
+
+const printLevelMessage = (level) => {
+  console.log(`\nGreat! You have selected the ${level.name} difficult level.`);
+  console.log(`You have ${level.chances} chances to guess the number.`);
+  console.log(`Let's start the game!`);
+}
+
+const runAttemps = ({chances, thoughtNumber}) => {
   let attemp = 1;
   let numberIsGuessed = false;
 
   do {
     const guess = readLineSync.question(('\nEnter your guess: '));
-    if (thinkedNumber === Number(guess)) {
+    if (thoughtNumber === Number(guess)) {
       console.log(`Congratulations! You guessed the correct number in ${attemp} attempts.`);
       numberIsGuessed = true;
       chances = 0;
     } else {
-      console.log(`Incorrect! The number is ${thinkedNumber < guess ? 'less': 'greater'} than ${guess}.`);
+      console.log(`Incorrect! The number is ${thoughtNumber < guess ? 'less': 'greater'} than ${guess}.`);
       chances = chances - 1;
       attemp = attemp + 1;
     }
@@ -59,8 +52,15 @@ const main = () => {
 
   if (!numberIsGuessed && chances === 0) {
     console.log(`I am sorry your attemps have ended!`);
-    console.log(`Do you want to try again?`);
   }
+}
+ 
+const main = () => {
+  const thoughtNumber = getRandomNumber();
+  greet();  
+  const level = chooseLevel();
+  printLevelMessage(level);
+  runAttemps({chances: level.chances, thoughtNumber});
 };
 
 module.exports = {
---
+++ src/lib/constanst.lib.js
@@ -0,0 +1,18 @@
+const LEVELS = {
+  1: {
+    name: 'Easy',
+    chances: 10
+  },
+  2: {
+    name: 'Medium',
+    chances: 5
+  },
+  3: {
+    name: 'Hard',
+    chances: 3
+  }
+};
+
+module.exports = {
+  LEVELS
+}
